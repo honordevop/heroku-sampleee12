2f632767cb791c5df4a475f9b3b7450c84f75073
honordevop <ogunladestephen20@gmail.com>
2022-02-13 01:21:04 +0100

    Working On deploying

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-//Install express server
+
 const express = require('express');
+
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+
 app.use(express.static(__dirname + '/dist/<demo-deploy>'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/<demo-deploy>/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/<demo-deploy>/' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);
